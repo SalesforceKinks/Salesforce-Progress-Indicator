585af7ac0d530db218dbcc956655af09fb094fff
Nikhil <salesforce.kinks@gmail.com>
2017-08-07 12:57:08 +0530

    Code for SLDS progress bar

---
+++ src/aura/SLDS_ProgressBar/SLDS_ProgressBarController.js
@@ -0,0 +1,11 @@
+({
+	goPrevious : function(component,event,helper) {
+     
+        helper.navigate(component,false);
+    },
+    
+    goNext : function(component,event,helper) {
+     	
+        helper.navigate(component,true);
+    },
+})
---
+++ src/aura/SLDS_ProgressBar/SLDS_ProgressBarHelper.js
@@ -0,0 +1,16 @@
+({
+	navigate : function(component, isNext) {
+     
+        var state = component.get("v.state");
+        
+        if(isNext) {
+            
+            state++;
+        } else {
+            
+            state--;
+        }
+        component.set("v.state",state);
+        component.set("v.percentage",parseInt((state-1)*25) + "%");
+    },
+})
